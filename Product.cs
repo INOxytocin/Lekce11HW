0a43039bab93e97acd9130179732d3f81d18aa2f
Petr <laisek@email.cz>
2025-01-06 02:29:49 +0100

    Sice jsem si musel hodně pomoci s dokumentací a StackOverflow, ale nějak to vyšlo. Začínám mít z toho velký guláš, když je kód složitější. Nechápu, jak někdo dokáže programovat s tisíciřádkovým kódem.

--- Product.cs
+++ Product.cs
@@ -1,81 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.Json;
 using Lekce11HW.Exceptions;
-
 
 namespace Lekce11HW
 {
     internal class Product
     {
-        int _price;
-        public string Name {  get; set; }
-        protected int Price
+        private int _price;
+        public string Name {get; set; }
+
+        public int Price
         {
             get => _price;
             set
             {
                 try
                 {
-                    if (_price < 0)
+                    if (value < 0)
                     {
-                        throw new InvalidProductException("The price of a product cannot be invalid");
+                        throw new InvalidProductException("The price of a product cannot be negative");
                     }
-                    else
-                    {
-                        _price = value;
-                    }
+                    _price = value;
                 }
                 catch(InvalidProductException ex)
                 {
                     Console.WriteLine("InvalidProductException " + ex.Message);
                     throw;
-
                 }
                 catch (Exception ex)
                 {
-
                     Console.WriteLine("Unknown Error " + ex.Message);
                     throw;
                 }
-                
             }
         }
+
         public int Quantity { get; set; }
 
-        public Product(string name, int price, int quantity) 
+        public Product(string name, int price, int quantity)
         {
-            Quantity = quantity;
             Name = name;
             try
             {
-                if (price >= 0)
-                {
-                    Price = price;
-                }
-                else { throw new InvalidProductException("The price of a product cannot be negative"); }
-                
+                Price = price; 
+                Quantity = quantity;
             }
-            catch(InvalidProductException ex) 
+            catch(InvalidProductException ex)
             {
                 Console.WriteLine("Invalid constructor argument " + ex.Message);
-                throw;
             }
             catch (Exception ex)
             {
-
                 Console.WriteLine("Unknown Error " + ex.Message);
-                throw;
             }
-            
         }
-        public Product()
-        {
 
+        public Product() 
+        {
         }
+
         public override string ToString()
         {
             return "Product Name: " + Name + " | Price: " + Price + " | Quantity: " + Quantity;
